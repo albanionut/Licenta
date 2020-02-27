14772b0bd189ffeff613a84fd17f4234bb3e1cbd
alban-ionut <ionut.m.alban@gmail.com>
2020-02-27 16:46:13 +0200

    2bars and swipe implemented

--- Assets/Scripts/Swipe.cs
+++ Assets/Scripts/Swipe.cs
@@ -3,55 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class Swipe : MonoBehaviour
 {
-    public float maxTime;
-    public float minSwipeDist;
-    
-    float startTime;
-    float endTime;
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
+    public GameObject bara1;
+    public GameObject bara2;
+    private GameObject[] listBar;
 
-    Vector3 startPos;
-    Vector3 endPos;
-    float swipeDistance;
-    float swipeTime;
-    
     void Start()
     {
-        
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
     }
 
-    
     void Update()
     {
-        if (Input.touchCount > 0 ){
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            UnityEngine.Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
 
-            UnityEngine.Touch touch = Input.GetTouch(0);
-            if(touch.phase == TouchPhase.Began){
-                startTime=Time.time;
-                startPos=touch.position;
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            Debug.Log("Right Swipe");
+                            bara1.SetActive(true);
+                            bara2.SetActive(false);
 
-            }else if(touch.phase == TouchPhase.Ended){
-                endTime=Time.time;
-                endPos=touch.position;
 
-                swipeDistance=(endPos - startPos).magnitude;
-                swipeTime=endTime-startTime;
+                        }
+                        else
+                        {   //Left swipe
+                            Debug.Log("Left Swipe");
+                            bara1.SetActive(false);
+                            bara2.SetActive(true);
 
-                if(swipeTime<maxTime && swipeDistance < minSwipeDist){
-                    swipe();
+                            /*listBar = GetComponent<BarList>().returnBarList();
+
+                            for (int i = 0; i < listBar.Length; i++)
+                            {
+                                if (listBar[i].active)
+                                {
+                                    listBar[i].SetActive(false);
+                                    listBar[i+1].SetActive(true);
+                                }
+
+                            }
+                            */
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            Debug.Log("Up Swipe");
+                        }
+                        else
+                        {   //Down swipe
+                            Debug.Log("Down Swipe");
+                        }
+                    }
                 }
-
-
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                }
             }
         }
-        
     }
 
-    void swipe(){
-        Vector2 distance = endPos - startPos;
-        if(Mathf.Abs(distance.x) > Mathf.Abs(distance.y) ){
-                Debug.Log("Horizontal Swipe");
-        }
-    }
 }
